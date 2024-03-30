10f78fac8536556455c11be97e4412af46285224
kalyanbabuaddepalli <babukalyan37@gmail.com>
2024-03-30 17:21:10 +0530

    first commit

---
+++ kalyan/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8672772447740ea5b2df78a14815bd6a34aad25b017daed48b8050e7f6e6ba2c
+size 1209
---
+++ kalyan/framework.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bd9caafc23e6c56e83879e62d0df57cae3dc0873f93ac875065995f53f47e4fc
+size 1044
